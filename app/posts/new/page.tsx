aa1f1f41b641f6a822698bcaf9d16e8dd811d587
faun3 <antonioculcea@gmail.com>
2023-08-13 19:43:29 +0300

    add zod validation that works (I hope)

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -1,18 +1,32 @@
+"use client";
+
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const Post = z.object({
   title: z.string().min(5).max(30),
   postText: z.string().min(30).max(500),
 });
 
+type Post = z.infer<typeof Post>;
+
 const NewPost = () => {
   const formData = {};
+
+  const { register, handleSubmit } = useForm<Post>({
+    resolver: zodResolver(Post),
+  });
+
+  const submitData = (data: Post) => {
+    console.log("valid", data);
+  };
 
   return (
     <div className="my-auto flex justify-center">
       <form
         className="flex flex-col gap-8 w-[90%] md:w-[65%]"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit(submitData)}
       >
         <div>
           <label htmlFor="title" className="block">
@@ -20,10 +34,10 @@ const NewPost = () => {
           </label>
           <input
             type="text"
-            name="title"
             id="title"
             placeholder="How to make Honey - a practical guide."
             className="w-full"
+            {...register("title")}
           />
         </div>
         <div>
@@ -31,11 +45,11 @@ const NewPost = () => {
             Your post lives here
           </label>
           <textarea
-            name="postText"
             id="postText"
             cols={30}
             rows={10}
             className="w-full"
+            {...register("postText")}
           ></textarea>
         </div>
         <button>Post</button>
