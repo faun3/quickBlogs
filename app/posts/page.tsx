09926b4f2bcf314727eefa31493b824c18b0320e
faun3 <antonioculcea@gmail.com>
2023-08-13 18:29:31 +0300

    added page.tsx for the /pages route

---
+++ app/posts/page.tsx
@@ -0,0 +1,12 @@
+const Posts = () => {
+  return (
+    <main>
+      <h1>Learn cool stuff fast</h1>
+      <h2>
+        Get inspired by interesting things people post, and join to share some
+        of what you know
+      </h2>
+    </main>
+  );
+};
+export default Posts;
